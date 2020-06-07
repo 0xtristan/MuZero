Rather large update: - Fixed masking indexing to include the terminal state - Updated masking to experiment with tf.boolean mask - Reverted boolean mask for absorbing states (no masking) with random absorbing policy/actions - Added logging of predictions during test

debug.py
@@ -1,22 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print(tf.config.experimental.list_physical_devices())
-
 from muzero.config import MuZeroConfig
 from muzero.main import Muzero
-
+from muzero.common import KnownBounds
 import ray
-import gym
 
-e = gym.make('CartPole-v0')
-
-print(e.observation_space) # Cart Position, Cart Velocity, Pole Angle, Velocity at Tip
-print(e.action_space) # Push Cart Left/Right
-
-gpu_count = len(tf.config.experimental.list_physical_devices('GPU'))
-
-def make_cartpole_config() -> MuZeroConfig:
+def make_atari_config() -> MuZeroConfig:
 
     def visit_softmax_temperature(num_moves, training_steps):
         if training_steps < 500e3:
@@ -27,29 +14,29 @@ def make_cartpole_config() -> MuZeroConfig:
             return 0.25
 
     return MuZeroConfig(
-        gym_env_name='CartPole-v0',
+        gym_env_name='CartPole-v1',
         action_space_size=2,
-        value_support_size=40,
+        value_support_size=10,
+        reward_support_size=10,
         selfplay_iterations=100, # Todo: implement None for continuous play
-        max_moves=27,#000,  # Half an hour at action repeat 4.
+        max_moves=500,
         discount=0.997,
         use_TD_values=True,
         dirichlet_alpha=0.25,
         num_simulations=50,
-        batch_size=10,#24,#1024,
-        td_steps=10,
-        num_actors=1,#350,
-        lr_init=0.05,
+        batch_size=128,#1024,
+        td_steps=10,#10
+        num_actors=1,#350
+        lr_init=0.05,#0.05
         lr_decay_steps=350e3,
-        checkpoint_interval=100,
+        checkpoint_interval=10,
         visit_softmax_temperature_fn=visit_softmax_temperature,
-        num_train_gpus=gpu_count)
+        # known_bounds=KnownBounds(min=0, max=500),
+        num_train_gpus=0)
 
+ray.init(local_mode=False)
 
-ray.init()
-config = make_cartpole_config()
+config = make_atari_config()
 mz = Muzero(config)
 
 mz.run()
-
-# I reckon we currently only have support on value, as signoided should really work well for r in 0-1 regimes.

muzero/env.py
@@ -148,7 +148,7 @@ class Game(object):
         # returns len K+1 sequences
         target_values, target_rewards, target_policies = [], [], []
         # We use a boolean masking vector to indicate the end of a sequence (à la NLP)
-        mask = []
+        mask, policy_mask = [], []
         # K + 1 iterations
         for current_index in range(t, t + K + 1):
             ## Value Target z_{t+K} ##
@@ -160,33 +160,47 @@ class Game(object):
             # For simplicity the network always predicts the most recently received
             # reward, even for the initial representation network where we already
             # know this reward.
-            if current_index > 0 and current_index <= len(self.rewards):
+            if current_index > t and current_index <= len(self.rewards):
+                # self.rewards[i] is the reward received after the ith state
+                # self.rewards[i-1] is therefore the most recently received reward
                 last_reward = self.rewards[current_index - 1]
             else:
-                # For i=0 and i=K+1
+                # For i=t and i=t+K+1
+                # Be careful because we have to set r=0 for first target (not first gamestep) due to initial inference
                 last_reward = 0
 
             if current_index < len(self.root_values):
-#                 targets.append((value, last_reward, self.child_visits[current_index]))
                 target_values.append(value)
                 target_rewards.append(last_reward)
                 target_policies.append(self.child_visits[current_index])
-                mask.append(1)
+                policy_mask.append(1)
             else:
                 # States past the end are treated as absorbing states
-#                 targets.append((0, last_reward, []))
                 target_values.append(0)
                 target_rewards.append(last_reward) # 0
                 # Uniform policy: 1/len(actions) for all actions
                 # Todo: is this a valid thing to do? @Sholto review
-                # This is actually meaningless because the policy is using logits
-#                 uniform_policy = [1/self.config.action_space_size 
-#                                   for _ in range(self.config.action_space_size)]
-                dummy_policy = np.array([-1 for _ in range(self.config.action_space_size)],dtype='float32')
+                # This is probability targets not logits
+                # uniform_policy = [1/self.config.action_space_size
+                #                   for _ in range(self.config.action_space_size)]
+                dummy_policy = np.array([1/self.config.action_space_size for _ in range(self.config.action_space_size)], dtype='float32')
+                # dummy_policy = np.array([-1 for _ in range(self.config.action_space_size)],dtype='float32')
                 target_policies.append(dummy_policy)
+                policy_mask.append(0)
+
+            if current_index <= len(self.root_values):
+                mask.append(1)
+            else:
                 mask.append(0)
+
+            # v = [v0,v1,v2,0,0]
+            # r = [0,r0,r1,r2,0]
+            # p = [p0,p1,p2,_,_]
+            # m = [1,1,1,1,0]
+            # pm = [1,1,1,0,0]
+            # a = [_,a0,a1,a3,_] - first index is initial_inference and thus no action
                 
-        return target_values, target_rewards, target_policies, mask
+        return target_values, target_rewards, target_policies, mask, policy_mask
     
     def action_history(self) -> ActionHistory:
         return ActionHistory(self.history, self.action_space_size)

muzero/models.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, Dense, Add, ReLU
 from tensorflow.keras.activations import sigmoid
 from tensorflow.keras.models import Model
 from tensorflow_core.python.keras import regularizers
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Callable
 from abc import ABC, abstractmethod
 import ray
 
@@ -52,7 +52,7 @@ class Network(ABC):
     
     
 class Network_FC(Network):
-    def __init__(self, config, s_in=4, h_size=16, repr_size=4):
+    def __init__(self, config, s_in=4, h_size=64, repr_size=2):
         super().__init__()
         # Initialise a uniform network - should I init these networks explicitly?
         n_acts = config.action_space_size
@@ -68,7 +68,7 @@ class Network_FC(Network):
 
     def initial_inference(self, obs, convert_to_scalar = True) -> NetworkOutput:
         # representation + prediction function
-        # input: 32x80x80 observation # TODO-No?
+        # input: 32x80x80 observation for breakout
         state = self.h(obs)
         policy_logits, value = self.f(state)
 #         value = self.fv(state)
@@ -77,14 +77,14 @@ class Network_FC(Network):
         if convert_to_scalar:
             value = support_to_scalar(value, self.value_support_size)
             reward = support_to_scalar(reward, self.reward_support_size)
-        return NetworkOutput(value, reward, policy_logits, state) # drop batch dim with [0], state still has batch
+        return NetworkOutput(value, reward, policy_logits, state)
     
     def recurrent_inference(self, state, action, convert_to_scalar = True) -> NetworkOutput:
         # dynamics + prediction function
         # Input: hidden state nfx5x5
         # Concat/pad action to channel dim of states
         state_action = tf.concat([state,tf.expand_dims(action,axis=-1)], axis=-1)
-        next_state, reward =  self.g(state_action)
+        next_state, reward = self.g(state_action)
         policy_logits, value = self.f(next_state)
 #         value = self.fv(next_state)
 #         policy_logits = self.fa(next_state)
@@ -92,6 +92,17 @@ class Network_FC(Network):
             value = support_to_scalar(value, self.value_support_size)
             reward = support_to_scalar(reward, self.reward_support_size)
         return NetworkOutput(value, reward, policy_logits, next_state)
+
+    def cb_get_variables(self) -> Callable:
+        """Return a callback that return the trainable variables of the network."""
+
+        def get_variables():
+            networks = (self.f, self.g, self.h)
+            return [variables
+                    for variables_list in map(lambda n: n.weights, networks)
+                    for variables in variables_list]
+
+        return get_variables
     
 ### FC Tensorflow model definitions ###
 
@@ -100,7 +111,7 @@ def ReprNet_FC(input_shape, repr_size, h_size, regularizer):
     x = o
 #     x = Dense(h_size, kernel_regularizer=regularizer)(x)
 #     x = LeakyReLU()(x)
-    s = Dense(repr_size, kernel_regularizer=regularizer, activation='sigmoid')(x) # Since we have +ve and -ve positions, angles, velocities
+    s = Dense(repr_size, kernel_regularizer=regularizer)(x) # Since we have +ve and -ve positions, angles, velocities
     return Model(o, s)
 
 def DynaNet_FC(input_shape, repr_size, h_size, support_size, regularizer):
@@ -111,7 +122,8 @@ def DynaNet_FC(input_shape, repr_size, h_size, support_size, regularizer):
     x = Dense(h_size, kernel_regularizer=regularizer)(x)
     x = LeakyReLU()(x)
     
-    s_new = Dense(repr_size, kernel_regularizer=regularizer, activation='sigmoid')(x)
+    s_new = Dense(repr_size, kernel_regularizer=regularizer)(x)
+    # r = LeakyReLU()(s_new)
     r = Dense(support_size*2+1, kernel_regularizer=regularizer)(x) # rewards are 1 for each frame it stays upright, 0 otherwise
     return Model(s, [s_new, r])
 
@@ -120,8 +132,8 @@ def PredNet_FC(input_shape, num_actions, h_size, support_size, regularizer):
     x = s
 #     x = Dense(h_size, kernel_regularizer=regularizer)(x)
 #     x = LeakyReLU()(x)
-#     x = Dense(h_size, kernel_regularizer=regularizer)(x)
-#     x = LeakyReLU()(x)
+    x = Dense(h_size, kernel_regularizer=regularizer)(x)
+    x = LeakyReLU()(x)
     
     a = Dense(num_actions, kernel_regularizer=regularizer)(x) # policy should be logits
     v = Dense(support_size*2+1, kernel_regularizer=regularizer)(x) # This can be a large number
@@ -182,9 +194,9 @@ def scalar_to_support(x, support_size):
     dim0_indices = tf.expand_dims(tf.range(0,x.shape[0]), axis=1) # this is just 0,1,2,3
     lower_indices = tf.concat([dim0_indices, dim1_indices], axis=1)
 
-    supports = tf.scatter_nd(lower_indices, tf.squeeze(prob_lower), shape=(x.shape[0],2*support_size+1))
+    supports = tf.scatter_nd(lower_indices, tf.squeeze(prob_lower, axis=1), shape=(x.shape[0],2*support_size+1))
     higher_indices = tf.concat([dim0_indices, tf.clip_by_value(dim1_indices+1,0,2*support_size)], axis=1)
-    supports = tf.tensor_scatter_nd_add(supports, higher_indices, tf.squeeze(prob_upper))
+    supports = tf.tensor_scatter_nd_add(supports, higher_indices, tf.squeeze(prob_upper, axis=1))
     return supports
     
         

muzero/selfplay.py
@@ -48,8 +48,8 @@ def play_game(config: MuZeroConfig, network: Network, greedy_policy=False, rende
         # obtain a hidden state given the current observation.
         root = Node(config, action=None)
         current_observation = tf.expand_dims(game.make_image(-1), 0) # 1x80x80x32 tf.Tensor - needs dummy batch dim
-        expand_node(config, root, game.legal_actions(),
-                    network.initial_inference(current_observation))
+        network_output = network.initial_inference(current_observation)
+        expand_node(config, root, game.legal_actions(), network_output)
         add_exploration_noise(config, root)
 
         # We then run a Monte Carlo Tree Search using only action sequences and the
@@ -64,7 +64,8 @@ def play_game(config: MuZeroConfig, network: Network, greedy_policy=False, rende
             game.env.gym_env.render(mode='human')
         if greedy_policy: 
             if i==0: print()
-            print(f"Test step {i}: \u03BD = {game.root_values[-1]:.2f}, \u03C0 = {np.around(game.child_visits[-1],2)}, a = {action}, u = {reward}")
+            print(f"Test step {i}: \u03BD = {game.root_values[-1]:.2f}, \u03C0 = {np.around(game.child_visits[-1],2)}, u = {reward}, a = {action}")
+            print(f"Predictions:  v = {float(network_output.value):.2f}, p = {np.around(tf.nn.softmax(network_output.policy_logits)[0].numpy(), 2)}")
         i+=1
     if render:
         print(f"Game Reward: {total_reward}")

muzero/storage.py
@@ -37,6 +37,7 @@ class ReplayBuffer(object):
         target_rewards = []
         target_policies = []
         masks = []
+        policy_masks = []
         
         # Sample a batch size worth of games
         games = [self.sample_game() for _ in range(self.batch_size)]
@@ -46,15 +47,20 @@ class ReplayBuffer(object):
             observations.append(g.make_image(i))
             # Use -1 padding for actions, this should get masked anyway
             action_history = g.history[i:i + K]
-            action_history_padded = np.pad(action_history, (1, K-len(action_history)), 
-                                           constant_values=(-1,-1)).astype('float32')
+
+            def random_pad(vector, pad_width, iaxis, kwargs):
+                vector[:pad_width[0]] = -1 # np.random.randint(20, 30, size=pad_width[0])
+                vector[vector.size-pad_width[1]:] = np.random.choice(vector[pad_width[0]:], size=pad_width[1])
+
+            action_history_padded = np.pad(action_history, (1, K-len(action_history)), mode=random_pad).astype('float32')
             actions.append(action_history_padded)
             
-            z,u,pi,mask = g.make_target(i, K, td)
+            z,u,pi,mask,policy_mask = g.make_target(i, K, td)
             target_values.append(z)
             target_rewards.append(u)
             target_policies.append(pi)
             masks.append(mask)
+            policy_masks.append(policy_mask)
         
         return (
                 tf.stack(observations, axis=0),
@@ -62,7 +68,8 @@ class ReplayBuffer(object):
                 tf.expand_dims(tf.cast(tf.stack(target_values, axis=0),dtype=tf.float32),axis=-1),
                 tf.expand_dims(tf.stack(target_rewards, axis=0),axis=-1),
                 tf.stack(target_policies, axis=0),
-                tf.expand_dims(tf.cast(tf.stack(masks, axis=0),dtype=tf.float32),axis=-1)
+                tf.expand_dims(tf.cast(tf.stack(masks, axis=0),dtype=tf.float32),axis=-1),
+                tf.expand_dims(tf.cast(tf.stack(policy_masks, axis=0), dtype=tf.float32), axis = -1)
                )
 
     def sample_game(self) -> Game:

muzero/train.py
@@ -36,13 +36,13 @@ def train_network(config: MuZeroConfig, storage: SharedStorage,
     train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 
-    for i in range(config.training_steps): #tqdm(range(config.training_steps), desc='Training iter'):
+    for i in range(config.training_steps):
 
         if i % config.checkpoint_interval == 0:
             storage.save_weights.remote(i, network.get_weights())
         ray_id = replay_buffer.sample_batch.remote(config.num_unroll_steps, config.td_steps)
         batch = ray.get(ray_id)
-        vl, rl, pl, wrl, tl, fg, gg, hg, v_pred, r_pred, p_pred, v_targ, r_targ, p_targ, acts = train_step(optimizer, network, batch, config.weight_decay)
+        vl, rl, pl, wrl, tl, fg, gg, hg, v_pred, r_pred, p_pred, v_targ, r_targ, p_targ, acts = train_step(i, optimizer, network, batch, config.weight_decay)
         network.steps += 1
         progbar.update(i, values=[('Value loss', vl),
                                   ('Reward loss', rl),
@@ -119,8 +119,7 @@ def scale_gradient(tensor, scale):
         
         
 # @tf.function
-def train_step(optimizer: Optimizer, network: Network, batch,
-                                     weight_decay: float):
+def train_step(step: int, optimizer: Optimizer, network: Network, batch, weight_decay: float):
     """
     Batch is 3-tuple of:
     Observation (N,80,80,1) for atari or (N,4) for cartpole
@@ -144,44 +143,76 @@ def train_step(optimizer: Optimizer, network: Network, batch,
     reward_target_mean = tf.keras.metrics.Mean()
     policy_target_dist = []
 
-    observations, actions, target_values, target_rewards, target_policies, masks = batch
+    observations, actions, target_values, target_rewards, target_policies, masks, policy_masks = batch
     with tf.GradientTape() as f_tape, tf.GradientTape() as g_tape, tf.GradientTape() as h_tape:
         loss = 0
         K = actions.shape[1] # seqlen
         for k in range(K):
+            # Targets
+            z, u, pi, mask_, policy_mask_ = target_values[:, k], target_rewards[:, k], target_policies[:, k], masks[:, k], policy_masks[:, k]
+            mask, policy_mask = tf.squeeze(mask_), tf.squeeze(policy_mask_) # (N,) rather than (N,1)
+
             if k==0:
                 # Initial step, from the real observation.
                 value, reward, policy_logits, hidden_state = network.initial_inference(observations, convert_to_scalar=False)
                 gradient_scale = 1.0
             else:
                 # All following steps
+                # masked_actions = tf.boolean_mask(actions[:,k], mask, axis=0)
+                # hidden_state_masked = tf.boolean_mask(hidden_state, mask, axis=0)
                 value, reward, policy_logits, hidden_state = network.recurrent_inference(hidden_state, actions[:,k], convert_to_scalar = False)
                 gradient_scale = 1.0 / (K-1)
 
-            hidden_state = scale_gradient(hidden_state, 0.5)
+            hidden_state = scale_gradient(hidden_state, 0.5) # Todo: is this compatible with masking??
             
-            # Targets
-            z, u, pi, mask = target_values[:,k], target_rewards[:,k], target_policies[:,k], masks[:,k]
+            # Masking
+            # Be careful with masking, if all values are masked it returns len 0 tensor
+
+            z_masked = z#tf.boolean_mask(z, mask, axis=0)
+            u_masked = u#tf.boolean_mask(u, mask, axis=0)
+            pi_masked = pi#tf.boolean_mask(pi, policy_mask, axis=0) # policy mask is mask but rolled left by 1
+
+            value_masked = value#tf.boolean_mask(value, mask, axis=0)
+            reward_masked = reward#tf.boolean_mask(reward, mask, axis=0)
+            policy_logits_masked = policy_logits#tf.boolean_mask(policy_logits, policy_mask, axis=0)
+
+            # z_masked = z*mask_
+            # u_masked = u*mask_
+            # pi_masked = pi*mask_ # policy mask is mask but rolled left by 1
+            #
+            # value_masked = value*mask_
+            # reward_masked = reward*mask_
+            # policy_logits_masked = policy_logits*mask_
             
-            value_loss = ce_loss(value, scalar_to_support(z, network.value_support_size), mask)
-            reward_loss = ce_loss(reward, scalar_to_support(u, network.reward_support_size), mask)
-            policy_loss = ce_loss(policy_logits, pi, mask) #tf.linalg.matmul(pi, policy_logits, transpose_a=True, transpose_b=False)
-            combined_loss = 1.0*value_loss + 1.0*reward_loss + 1.0*policy_loss
-#             pdb.set_trace()
+            value_loss = ce_loss(value_masked, scalar_to_support(z_masked, network.value_support_size), mask)
+            reward_loss = ce_loss(reward_masked, scalar_to_support(u_masked, network.reward_support_size), mask)
+            policy_loss = ce_loss(policy_logits_masked, pi_masked, mask)
+            combined_loss = 0.25*value_loss + 1.0*reward_loss + 1.0*policy_loss
 
             loss += scale_gradient(combined_loss, gradient_scale)
+
+            if tf.math.is_nan(loss):
+                print("Loss is NaN")
+                pdb.set_trace()
             
             # Metric logging for tensorboard
             value_loss_metric(value_loss)
             reward_loss_metric(reward_loss)
             policy_loss_metric(policy_loss)
-            
-            value_pred_mean(support_to_scalar(value, network.value_support_size)*mask)
-            reward_pred_mean(support_to_scalar(reward, network.reward_support_size)*mask)
-            policy_pred_dist.append(tf.nn.softmax(policy_logits)*mask)
-            value_target_mean(z*mask)
-            reward_target_mean(u*mask)
-            policy_target_dist.append(pi*mask)
+
+            scalar_value = support_to_scalar(value_masked, network.value_support_size)
+            scalar_reward = support_to_scalar(reward_masked, network.reward_support_size)
+            policy_probs = tf.nn.softmax(policy_logits)
+
+            if (step+1)%100==0:
+                print("break")
+
+            value_pred_mean(scalar_value)
+            reward_pred_mean(scalar_reward)
+            policy_pred_dist.append(policy_probs*mask_)
+            value_target_mean(z_masked)
+            reward_target_mean(u_masked)
+            policy_target_dist.append(pi*mask_)
         
 #         total_loss_metric(loss)
 #         total_reward()
@@ -200,20 +231,22 @@ def train_step(optimizer: Optimizer, network: Network, batch,
     optimizer.apply_gradients(zip(f_grad, network.f.trainable_variables))
     optimizer.apply_gradients(zip(g_grad, network.g.trainable_variables))
     optimizer.apply_gradients(zip(h_grad, network.h.trainable_variables))
-    # We ought to average or sum these losses - otherwise reward loss is 0 at the end lol
-    return value_loss_metric.result(), reward_loss_metric.result(), policy_loss_metric.result(), weight_reg_loss, loss, f_grad, g_grad, h_grad, value_pred_mean.result(), reward_pred_mean.result(), policy_pred_dist, value_target_mean.result(), reward_target_mean.result(), policy_target_dist, actions
+
+    # optimizer.minimize(loss=loss, var_list=network.cb_get_variables())
+
+    return value_loss_metric.result(), reward_loss_metric.result(), policy_loss_metric.result(), weight_reg_loss, loss, f_grad, g_grad, h_grad, value_pred_mean.result(), reward_pred_mean.result(), policy_pred_dist, value_target_mean.result(), reward_target_mean.result(), policy_target_dist, actions[:,1:]
 
 
 # Use categorical/softmax cross-entropy loss rather than binary/logistic
 # Value and reward are non-logits, actions are logits
-def mse_loss(y_pred, y_true, mask) -> float:
+def mse_loss(y_pred, y_true) -> float:
     # MSE in board games, cross entropy between categorical values in Atari. 
     return tf.reduce_sum(tf.reduce_sum(tf.math.squared_difference(y_pred, y_true),axis=1)*tf.squeeze(mask)) / tf.reduce_sum(tf.squeeze(mask))
 
 def ce_loss(y_pred, y_true, mask) -> float:
-    # MSE in board games, cross entropy between categorical values in Atari. 
-#     return tf.reduce_mean(-y_true*tf.nn.log_softmax(y_pred, axis=None)*mask, axis=[0,1])
-#     pdb.set_trace()
-#     return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)*tf.squeeze(mask))
-    return  tf.math.divide_no_nan( tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)*tf.squeeze(mask)) , tf.reduce_sum(tf.squeeze(mask)) )  #tf.math.maximum(tf.reduce_sum(tf.squeeze(mask)),1)
-
+    # MSE in board games, cross entropy between categorical values in Atari.
+    # return  tf.math.divide_no_nan( tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)*mask) , tf.reduce_sum(mask) )
+    if y_pred.shape[0]==0 or y_true.shape[0]==0:
+        print("Entire batch masked")
+        return 0
+    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))