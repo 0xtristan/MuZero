Rather large update: - Fixed masking indexing to include the terminal state - Updated masking to experiment with tf.boolean mask - Reverted boolean mask for absorbing states (no masking) with random absorbing policy/actions - Added logging of predictions during test
- actions now OHE
- added hidden state scaling only for k > 0

muzero/mcts_numpy.py
@@ -138,7 +138,7 @@ def run_mcts(config: MuZeroConfig, root: Node, action_history: ActionHistory,
         # Predictions: f(s_k) = v_k, p_k
         # -> (v,r,p,s)
         network_output = network.recurrent_inference(parent.hidden_state,
-                                                     tf.cast(tf.expand_dims(history.last_action(),0),dtype=tf.float32)) # Needs batch dim and to be float
+                                                     tf.expand_dims(history.last_action(),0)) # Needs batch dim and to be float
         # expand node using v,r,p predictions from NN
         expand_node(config, node, history.action_space(), network_output)
 
@@ -152,7 +152,7 @@ def run_mcts(config: MuZeroConfig, root: Node, action_history: ActionHistory,
 # Select the child with the highest UCB score.
 def best_move(config: MuZeroConfig, node: Node, min_max_stats: MinMaxStats):
     action = np.argmax(ucb_score(config, node, min_max_stats)) # ucb_score should return a np.array
-    return action
+    return int(action)
 
 def maybe_add_child(config: MuZeroConfig, action: int, node: Node):
     if action not in node.children:

muzero/models.py
@@ -52,7 +52,7 @@ class Network(ABC):
     
     
 class Network_FC(Network):
-    def __init__(self, config, s_in=4, h_size=64, repr_size=2):
+    def __init__(self, config, s_in=4, h_size=32, repr_size=32):
         super().__init__()
         # Initialise a uniform network - should I init these networks explicitly?
         n_acts = config.action_space_size
@@ -62,9 +62,10 @@ class Network_FC(Network):
         self.f = PredNet_FC((repr_size,), n_acts, h_size, support_size=self.value_support_size, regularizer=self.regularizer)
 #         self.fv = PredNetV_FC((h_size,), n_acts, h_size, self.value_support_size)
 #         self.fa = PredNetA_FC((h_size,), n_acts, h_size)
-        self.g = DynaNet_FC((repr_size+1,), repr_size, h_size, support_size=self.reward_support_size, regularizer=self.regularizer)
+        self.g = DynaNet_FC((repr_size+config.action_space_size,), repr_size, h_size, support_size=self.reward_support_size, regularizer=self.regularizer)
         self.h = ReprNet_FC((s_in,), repr_size, h_size, regularizer=self.regularizer)
         self.steps = 0
+        self.config = config
 
     def initial_inference(self, obs, convert_to_scalar = True) -> NetworkOutput:
         # representation + prediction function
@@ -83,7 +84,8 @@ class Network_FC(Network):
         # dynamics + prediction function
         # Input: hidden state nfx5x5
         # Concat/pad action to channel dim of states
-        state_action = tf.concat([state,tf.expand_dims(action,axis=-1)], axis=-1)
+        action_ohe = tf.one_hot(action, self.config.action_space_size)
+        state_action = tf.concat([state,action_ohe], axis=1)
         next_state, reward = self.g(state_action)
         policy_logits, value = self.f(next_state)
 #         value = self.fv(next_state)
@@ -112,6 +114,7 @@ def ReprNet_FC(input_shape, repr_size, h_size, regularizer):
 #     x = Dense(h_size, kernel_regularizer=regularizer)(x)
 #     x = LeakyReLU()(x)
     s = Dense(repr_size, kernel_regularizer=regularizer)(x) # Since we have +ve and -ve positions, angles, velocities
+    s = min_max_scaling(s) # This replaces our activation fn
     return Model(o, s)
 
 def DynaNet_FC(input_shape, repr_size, h_size, support_size, regularizer):
@@ -123,6 +126,7 @@ def DynaNet_FC(input_shape, repr_size, h_size, support_size, regularizer):
     x = LeakyReLU()(x)
     
     s_new = Dense(repr_size, kernel_regularizer=regularizer)(x)
+    s_new = min_max_scaling(s_new)
     # r = LeakyReLU()(s_new)
     r = Dense(support_size*2+1, kernel_regularizer=regularizer)(x) # rewards are 1 for each frame it stays upright, 0 otherwise
     return Model(s, [s_new, r])
@@ -156,6 +160,12 @@ def PredNet_FC(input_shape, num_actions, h_size, support_size, regularizer):
 # #     x = LeakyReLU()(x)
 #     a = Dense(num_actions)(x) # policy should be logits
 #     return Model(s, a)
+
+def min_max_scaling(tensor, eps = 1e-12):
+    """ Rescales tensor linearly to range [0,1]. See appendix G of paper """
+    min_val = tf.reduce_min(tensor)
+    max_val = tf.reduce_max(tensor)
+    return (tensor - min_val + eps) / tf.maximum((max_val - min_val), 2 * eps)
 
 def support_to_scalar(logits, support_size, eps = 0.001):
     """

muzero/selfplay.py
@@ -64,7 +64,7 @@ def play_game(config: MuZeroConfig, network: Network, greedy_policy=False, rende
             game.env.gym_env.render(mode='human')
         if greedy_policy: 
             if i==0: print()
-            print(f"Test step {i}: \u03BD = {game.root_values[-1]:.2f}, \u03C0 = {np.around(game.child_visits[-1],2)}, u = {reward}, a = {action}")
+            print(f"Test step {i}: \u03BD = {game.root_values[-1]:.2f}, \u03C0 = {np.around(game.child_visits[-1],2)}, a = {action}")
             print(f"Predictions:  v = {float(network_output.value):.2f}, p = {np.around(tf.nn.softmax(network_output.policy_logits)[0].numpy(), 2)}")
         i+=1
     if render:

muzero/storage.py
@@ -52,7 +52,7 @@ class ReplayBuffer(object):
                 vector[:pad_width[0]] = -1 # np.random.randint(20, 30, size=pad_width[0])
                 vector[vector.size-pad_width[1]:] = np.random.choice(vector[pad_width[0]:], size=pad_width[1])
 
-            action_history_padded = np.pad(action_history, (1, K-len(action_history)), mode=random_pad).astype('float32')
+            action_history_padded = np.pad(action_history, (1, K-len(action_history)), mode=random_pad)#.astype('int16') # keep it signed because I use -1
             actions.append(action_history_padded)
             
             z,u,pi,mask,policy_mask = g.make_target(i, K, td)
@@ -64,7 +64,7 @@ class ReplayBuffer(object):
         
         return (
                 tf.stack(observations, axis=0),
-                tf.stack(actions, axis=0),
+                tf.cast(tf.stack(actions, axis=0), dtype=tf.int32),
                 tf.expand_dims(tf.cast(tf.stack(target_values, axis=0),dtype=tf.float32),axis=-1),
                 tf.expand_dims(tf.stack(target_rewards, axis=0),axis=-1),
                 tf.stack(target_policies, axis=0),

muzero/train.py
@@ -50,9 +50,7 @@ def train_network(config: MuZeroConfig, storage: SharedStorage,
                                   ('Weight Reg loss', wrl),
                                   ('Total loss', tl),
                                  ])
-        if (i+1)%20==0:
-            _,total_reward = play_game(config, network, greedy_policy=True, render=False)
-            
+
         with train_summary_writer.as_default():
             tf.summary.scalar('1. Losses/Value loss', vl, step=i)
             tf.summary.scalar('1. Losses/Reward loss', rl, step=i)
@@ -74,6 +72,7 @@ def train_network(config: MuZeroConfig, storage: SharedStorage,
             
             tf.summary.histogram('Action distribution', acts, step=i)
             if (i+1)%20==0:
+                _, total_reward = play_game(config, network, greedy_policy=True, render=False)
                 tf.summary.scalar('Reward', total_reward, step=i)
 
     storage.save_weights.remote(config.training_steps, network.get_weights())
@@ -83,40 +82,7 @@ def scale_gradient(tensor, scale):
     """Scales the gradient for the backward pass."""
     return tensor * scale + tf.stop_gradient(tensor) * (1 - scale)
 
-
-# # BPTT - this code can surely be vectorised - yes it really does rather than looping over batches
-# def update_weights(optimizer: Optimizer, network: Network, batch,
-#                                      weight_decay: float):
-#     loss = 0
-#     for image, actions, targets in batch:
-#         # Initial step, from the real observation.
-#         value, reward, policy_logits, hidden_state = network.initial_inference(image)
-#         predictions = [(1.0, value, reward, policy_logits)]
-
-#         # Recurrent steps, from action and previous hidden state.
-#         for action in actions:
-#             value, reward, policy_logits, hidden_state = network.recurrent_inference(hidden_state, action)
-#             predictions.append((1.0 / len(actions), value, reward, policy_logits))
-
-#             hidden_state = scale_gradient(hidden_state, 0.5)
-
-#         for prediction, target in zip(predictions, targets):
-#             gradient_scale, value, reward, policy_logits = prediction
-#             target_value, target_reward, target_policy = target
-
-#             l = (
-#                 scalar_loss(value, target_value) + # value
-#                 scalar_loss(reward, target_reward) + # reward
-#                 cce_loss_logits(policy_logits, target_policy) # action
-#             )
-
-#             loss += scale_gradient(l, gradient_scale)
-
-#     for weights in network.get_weights():
-#         loss += weight_decay * tf.nn.l2_loss(weights)
-
-#     optimizer.minimize(loss)
-        
+# def test_step()
         
 # @tf.function
 def train_step(step: int, optimizer: Optimizer, network: Network, batch, weight_decay: float):
@@ -162,8 +128,7 @@ def train_step(step: int, optimizer: Optimizer, network: Network, batch, weight_
                 # hidden_state_masked = tf.boolean_mask(hidden_state, mask, axis=0)
                 value, reward, policy_logits, hidden_state = network.recurrent_inference(hidden_state, actions[:,k], convert_to_scalar = False)
                 gradient_scale = 1.0 / (K-1)
-
-            hidden_state = scale_gradient(hidden_state, 0.5) # Todo: is this compatible with masking??
+                hidden_state = scale_gradient(hidden_state, 0.5) # Todo: is this compatible with masking??
             
             # Masking
             # Be careful with masking, if all values are masked it returns len 0 tensor
@@ -187,7 +152,7 @@ def train_step(step: int, optimizer: Optimizer, network: Network, batch, weight_
             value_loss = ce_loss(value_masked, scalar_to_support(z_masked, network.value_support_size), mask)
             reward_loss = ce_loss(reward_masked, scalar_to_support(u_masked, network.reward_support_size), mask)
             policy_loss = ce_loss(policy_logits_masked, pi_masked, mask)
-            combined_loss = 0.25*value_loss + 1.0*reward_loss + 1.0*policy_loss
+            combined_loss = 1.0*value_loss + 1.0*reward_loss + 1.0*policy_loss
 
             loss += scale_gradient(combined_loss, gradient_scale)
 